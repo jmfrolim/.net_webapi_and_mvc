bb159ac4355fba83964f25f21c53f98474650fa2
Joao Manoel <joao.mfreitas@live.com>
2019-06-17 20:02:51 -0300

    Add project files.

---
+++ LabTeste_MVC_API/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="LabTeste_MVC_API.MvcApplication" Language="C#" %>
---
+++ LabTeste_MVC_API/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(LabTeste_MVC_API.Startup))]
+namespace LabTeste_MVC_API
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
